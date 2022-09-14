2aeffe7cea073d654fde44b2d13a00c7a16220f5
Pikatan Arya Bramajati <pikatanab26@gmail.com>
2022-09-14 22:25:24 +0700

    Tambah semuanya

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+data_catalog_item = CatalogItem.objects.all()
+context = {
+    "catalog_item_list": data_catalog_item,
+    "name": "Pikatan Arya Bramajati",
+    "npm": "2106630031"
+}
