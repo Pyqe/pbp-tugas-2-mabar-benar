2f0f36e451d70a30fd2b771d7a36734080644aba
Pikatan Arya Bramajati <pikatanab26@gmail.com>
2022-09-14 23:01:55 +0700

    Lakukan migration

--- katalog/views.py
+++ katalog/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from katalog.models import CatalogItem
 
 def show_katalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        "catalog_item_list": data_catalog_item,
+        "name": "Pikatan Arya Bramajati",
+        "npm": "2106630031"
+    }
     return render(request, "katalog.html", context)
-
-data_catalog_item = CatalogItem.objects.all()
-context = {
-    "catalog_item_list": data_catalog_item,
-    "name": "Pikatan Arya Bramajati",
-    "npm": "2106630031"
-}
