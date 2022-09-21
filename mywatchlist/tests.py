2b0e6a3b2b75ad5040d9dde74239452ba8d80e05
Pikatan Arya Bramajati <pikatanab26@gmail.com>
2022-09-22 00:43:15 +0700

    Tambah tests.py

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+import unittest
+from django.test import Client
 
-# Create your tests here.
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_details(self):
+        response = self.client.get('html/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('xml/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('json/')
+        self.assertEqual(response.status_code, 200)
