1dede9a866fd9394aedc5410688d380f046018cd
Pikatan Arya Bramajati <pikatanab26@gmail.com>
2022-09-28 18:50:49 +0700

    Coba benarkan error

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,7 +3,7 @@ from todolist.views import show_todolist, login_user, logout_user, register
 
 app_name = "todolist"
 
-urlspatterns = [
+urlpatterns = [
     path("", show_todolist, name = "show_todolist"),
     path("login/", login_user, name = "login_user"),
     path("logout/", logout_user, name = "logout_user"),
